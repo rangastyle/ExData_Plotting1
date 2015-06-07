232a42c5e8cd96afa904f08d09f14eb9619a4fc7
rangastyle <ranganaigwati@gmail.com>
2015-06-07 08:40:31 -0700

    Added R scripts and png files

--- figure/plot1.R
+++ figure/plot1.R
@@ -1,30 +1,21 @@
 setwd("C:/Users/Sony LapTop User/Dropbox/CourseraProgrammingClass/Exploratory Data Analysis/Project 1")
-fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
-#Download zip file from website and load it into R
+#Download zip file from website and load relevant subset into R
+fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 destination <- "file.zip"
 download.file(url=fileUrl, destfile=destination, mode="wb") 
-dat<- read.table(unz("file.zip","household_power_consumption.txt"),header=TRUE,sep=";")  
 
-#######################
-install.packages("sqldf")
-#library(RSQLite.extfuns)
+#install.packages("sqldf")
 library(sqldf)
 dat3 <- read.csv.sql(file="household_power_consumption.txt",sql = "select * from file where Date in ('1/2/2007', '2/2/2007')",header=TRUE,sep=";")
 closeAllConnections()
 
-head(dat3)
-tail(dat3)
-dim(dat3)
 
-names(dat3)
-summary(dat3)
-#head(dat3[dat3$Global_active_power=="?",])
-
+#Plot histogram and save file as a png file
 png(filename = "plot1.png",
     width = 480, height = 480, units = "px")
 
 hist(dat3$Global_active_power,breaks ="sturges",col="red",main="Global Active Power"
-,ylab="", xlab="Global Active Power(killowatts)")
+,ylab="Frequency", xlab="Global Active Power(killowatts)")
 
 dev.off()
